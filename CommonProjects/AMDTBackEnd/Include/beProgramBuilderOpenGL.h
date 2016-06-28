111b5ba757469d8752ef6a24a9d07441750e9ee7
AmitBM <amit.bm3@gmail.com>
2016-06-28 16:02:16 +0300

    Update the OpenGL device IDs to produce more accurate ISA and performance statistics results.

--- CommonProjects/AMDTBackEnd/Include/beProgramBuilderOpenGL.h
+++ CommonProjects/AMDTBackEnd/Include/beProgramBuilderOpenGL.h
@@ -93,8 +93,10 @@ public:
     /// Extracts the OpenGL version of the installed runtime.
     bool GetOpenGLVersion(gtString& glVersion);
 
-private:
+    /// Retrieve the device ID and Revision ID from the OpenGL backend.
+    bool GetDeviceGLInfo(const std::string& deviceName, size_t& deviceFamilyId, size_t& deviceRevision) const;
 
+private:
     std::set<std::string> m_publicDevices;
 };
 
